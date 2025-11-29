1278b8c7196edf203243b8b86f16c8bc2301185e
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-11-29 14:56:11 +0100

    Rotas do solicitante e entregador  atualizadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router-dom";
+//Site
 import Home from "../pages/site/Home";
 import NotFound from "../pages/site/NotFound";
 
+//Autenticação
 import Login from "../pages/auth/Login";
 import CadastrarSe from "../pages/auth/CadastrarSe";
 import CadastrarSolicitante from "../pages/auth/CadastrarSolicitante";
@@ -9,6 +11,12 @@ import CadastrarEntregador from "../pages/auth/CadastrarEntregador";
 import EsqueceuSenha from "../pages/auth/EsqueceuSenha";
 import RedefinirSenha from "../pages/auth/RedefinirSenha";
 import VerifiqueEmail from "../pages/auth/VerifiqueEmail";
+
+//Solicitante
+import DashboardSolicitante from "../pages/solicitante/Dashboard"
+
+//Entregador
+import DashboardEntregador from "../pages/entregador/Dashboard"
 
 export default function AppRoutes () {
     return (
@@ -25,6 +33,12 @@ export default function AppRoutes () {
             <Route path="/auth/esqueceu-senha" element={<EsqueceuSenha />} />
             <Route path="/auth/redefinir-senha" element={<RedefinirSenha />} />
             <Route path="/auth/verifique-email" element={<VerifiqueEmail />} />
+
+            {/*Rotas do solicintante */}
+            <Route path="/dashboard/solicitante/" element={<DashboardSolicitante />} />
+
+            {/*Rotas do entregador */}
+            <Route path="/dashboard/entregador/" element={<DashboardEntregador />} />
         </Routes>
     )
 }
