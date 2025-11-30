e5757d1515608ffbcf06c71cc3d5ae4ff433bd8c
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-11-30 16:09:40 +0100

    Rotas do solicitante agrupado

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -14,6 +14,14 @@ import VerifiqueEmail from "../pages/auth/VerifiqueEmail";
 
 //Solicitante
 import DashboardSolicitante from "../pages/solicitante/Dashboard"
+import MeusPedidos from "../pages/solicitante/MeusPedidos"
+import CriarNovoPedido from "../pages/solicitante/CriarNovoPedido"
+import EditarPedido from "../pages/solicitante/EditarPedido"
+import DetalhesDoPedido from "../pages/solicitante/DetalhesDoPedido"
+import HistoricoPedidos from "../pages/solicitante/Historico"
+import Configuracoes from "../pages/solicitante/Configuracoes"
+import NotificacoesSolicitante from "../pages/solicitante/Notificacoes"
+import NotFoundSolicitante from "../pages/solicitante/NotFoundSolicitante"
 
 //Entregador
 import DashboardEntregador from "../pages/entregador/Dashboard"
@@ -35,7 +43,19 @@ export default function AppRoutes () {
             <Route path="/auth/verifique-email" element={<VerifiqueEmail />} />
 
             {/*Rotas do solicintante */}
-            <Route path="/dashboard/solicitante/" element={<DashboardSolicitante />} />
+            <Route path="/dashboard/solicitante/">
+                <Route path="" element={<DashboardSolicitante />} />
+                <Route path="pedidos" element={<MeusPedidos />} />
+                <Route path="novo-pedido" element={<CriarNovoPedido />} />
+                <Route path="editar-pedido" element={<EditarPedido />} />
+                <Route path="detalhes-pedido" element={<DetalhesDoPedido />} />
+                <Route path="historico" element={<HistoricoPedidos />} />
+                <Route path="configuracoes" element={<Configuracoes />} />
+                <Route path="notificacoes" element={<NotificacoesSolicitante />} />
+                
+                {/* ROTA NOT FOUND ESPECÍFICA PARA O SOLICITANTE */}
+                <Route path="*" element={<NotFoundSolicitante />} />
+            </Route>
 
             {/*Rotas do entregador */}
             <Route path="/dashboard/entregador/" element={<DashboardEntregador />} />
