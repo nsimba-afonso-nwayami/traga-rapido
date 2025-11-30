53d6ecc8380681fa8818cf0acc49c7bc34b0813e
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-11-30 16:05:19 +0100

    estilo do mapa leaflet adicionado

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'leaflet/dist/leaflet.css';
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "react-hot-toast";
