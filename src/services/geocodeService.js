e9e58661b79a84e40fd3c20c6a45e25412db7a2d
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2025-12-02 11:15:17 +0000

    Geo lat e long Atualizado

--- src/services/geocodeService.js
+++ src/services/geocodeService.js
@@ -1,4 +1,4 @@
-/*export async function geocodeAddress(address) {
+export async function geocodeAddress(address) {
   const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(
     address
   )}&format=json&limit=1`;
@@ -14,21 +14,4 @@
     lat: parseFloat(data[0].lat),
     lon: parseFloat(data[0].lon),
   };
-}*/
-
-export async function geocodeAddress(address) {
-  const url = `/geocode/search?q=${encodeURIComponent(address)}&format=json&limit=1`;
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  if (!data.length) {
-    throw new Error("Endereço não encontrado.");
-  }
-
-  return {
-    lat: parseFloat(data[0].lat),
-    lon: parseFloat(data[0].lon),
-  };
 }
-
