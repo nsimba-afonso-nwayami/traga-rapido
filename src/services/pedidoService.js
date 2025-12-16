8051c5d99a0b821e6944e6d5b767ec7f315030ab
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-12-17 00:36:37 +0100

    Pedido Service feito

--- src/services/pedidoService.js
+++ src/services/pedidoService.js
@@ -3,3 +3,18 @@ import { api } from "./api";
 export async function criarPedido(payload) {
   return api.post("/pedidos/", payload);
 }
+
+export async function listarPedidos() {
+  return api.get("/pedidos/");
+}
+
+// Lista apenas pedidos do solicitante
+export async function listarPedidosPorSolicitante(idSolicitante) {
+  const response = await listarPedidos();
+  return response.data.filter(pedido => pedido.solicitante === idSolicitante);
+}
+
+// Elimina pedido
+export async function eliminarPedido(id) {
+  return api.delete(`/pedidos/${id}/`);
+}
