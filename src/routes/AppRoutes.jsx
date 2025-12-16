2fb3e57f548b375426501269e65cee4fc94ef6f3
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2025-12-16 14:01:10 +0000

    Rotas privadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-//Site
+import RolePrivateRoute from "./RolePrivateRoute"; // novo
+
+// Site
 import Home from "../pages/site/Home";
 import NotFound from "../pages/site/NotFound";
 
-//Autenticação
+// Auth
 import Login from "../pages/auth/Login";
 import CadastrarSe from "../pages/auth/CadastrarSe";
 import CadastrarSolicitante from "../pages/auth/CadastrarSolicitante";
@@ -12,67 +14,71 @@ import EsqueceuSenha from "../pages/auth/EsqueceuSenha";
 import RedefinirSenha from "../pages/auth/RedefinirSenha";
 import VerifiqueEmail from "../pages/auth/VerifiqueEmail";
 
-//Solicitante
-import DashboardSolicitante from "../pages/solicitante/Dashboard"
-import MeusPedidos from "../pages/solicitante/MeusPedidos"
-import CriarNovoPedido from "../pages/solicitante/CriarNovoPedido"
-import EditarPedido from "../pages/solicitante/EditarPedido"
-import DetalhesDoPedido from "../pages/solicitante/DetalhesDoPedido"
-import HistoricoPedidos from "../pages/solicitante/Historico"
-import Configuracoes from "../pages/solicitante/Configuracoes"
-import NotificacoesSolicitante from "../pages/solicitante/Notificacoes"
-import NotFoundSolicitante from "../pages/solicitante/NotFoundSolicitante"
+// Solicitante
+import DashboardSolicitante from "../pages/solicitante/Dashboard";
+import MeusPedidos from "../pages/solicitante/MeusPedidos";
+import CriarNovoPedido from "../pages/solicitante/CriarNovoPedido";
+import EditarPedido from "../pages/solicitante/EditarPedido";
+import DetalhesDoPedido from "../pages/solicitante/DetalhesDoPedido";
+import HistoricoPedidos from "../pages/solicitante/Historico";
+import Configuracoes from "../pages/solicitante/Configuracoes";
+import NotificacoesSolicitante from "../pages/solicitante/Notificacoes";
+import NotFoundSolicitante from "../pages/solicitante/NotFoundSolicitante";
 
-//Entregador
-import DashboardEntregador from "../pages/entregador/Dashboard"
-import ListaDePedidos from "../pages/entregador/ListaDePedidos"
-import DetalhesCorrida from "../pages/entregador/DetalhesCorrida"
-import HistoricoEntregador from "../pages/entregador/HistoricoEntregador"
-import NotificacoesEntregador from "../pages/entregador/NotificacoesEntregador"
-import ConfiguracoesEntregador from "../pages/entregador/ConfiguracoesEntregador"
-import NotFoundEntregador from "../pages/entregador/NotFoundEntregador"
+// Entregador
+import DashboardEntregador from "../pages/entregador/Dashboard";
+import ListaDePedidos from "../pages/entregador/ListaDePedidos";
+import DetalhesCorrida from "../pages/entregador/DetalhesCorrida";
+import HistoricoEntregador from "../pages/entregador/HistoricoEntregador";
+import NotificacoesEntregador from "../pages/entregador/NotificacoesEntregador";
+import ConfiguracoesEntregador from "../pages/entregador/ConfiguracoesEntregador";
+import NotFoundEntregador from "../pages/entregador/NotFoundEntregador";
 
-export default function AppRoutes () {
-    return (
-        <Routes>
-            {/*Rotas do site */}
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<NotFound />} />
+export default function AppRoutes() {
+  return (
+    <Routes>
+      {/* Site */}
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
 
-            {/*Rotas de login */}
-            <Route path="/auth/login" element={<Login />} />
-            <Route path="/auth/criar-conta" element={<CadastrarSe />} />
-            <Route path="/auth/cadastrar-solicitante" element={<CadastrarSolicitante />} />
-            <Route path="/auth/cadastrar-entregador" element={<CadastrarEntregador />} />
-            <Route path="/auth/esqueceu-senha" element={<EsqueceuSenha />} />
-            <Route path="/auth/redefinir-senha" element={<RedefinirSenha />} />
-            <Route path="/auth/verifique-email" element={<VerifiqueEmail />} />
+      {/* Auth */}
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/criar-conta" element={<CadastrarSe />} />
+      <Route path="/auth/cadastrar-solicitante" element={<CadastrarSolicitante />} />
+      <Route path="/auth/cadastrar-entregador" element={<CadastrarEntregador />} />
+      <Route path="/auth/esqueceu-senha" element={<EsqueceuSenha />} />
+      <Route path="/auth/redefinir-senha" element={<RedefinirSenha />} />
+      <Route path="/auth/verifique-email" element={<VerifiqueEmail />} />
 
-            {/*Rotas do solicitante */}
-            <Route path="/dashboard/solicitante/">
-                <Route path="" element={<DashboardSolicitante />} />
-                <Route path="pedidos" element={<MeusPedidos />} />
-                <Route path="novo-pedido" element={<CriarNovoPedido />} />
-                <Route path="editar-pedido" element={<EditarPedido />} />
-                <Route path="detalhes-pedido" element={<DetalhesDoPedido />} />
-                <Route path="historico" element={<HistoricoPedidos />} />
-                <Route path="configuracoes" element={<Configuracoes />} />
-                <Route path="notificacoes" element={<NotificacoesSolicitante />} />
-                
-                <Route path="*" element={<NotFoundSolicitante />} />
-            </Route>
+      {/* ROTAS PROTEGIDAS POR TIPO DE USUÁRIO */}
+      
+      {/* Solicitante */}
+      <Route element={<RolePrivateRoute allowedRoles={["SOLICITANTE"]} />}>
+        <Route path="/dashboard/solicitante">
+          <Route index element={<DashboardSolicitante />} />
+          <Route path="pedidos" element={<MeusPedidos />} />
+          <Route path="novo-pedido" element={<CriarNovoPedido />} />
+          <Route path="editar-pedido" element={<EditarPedido />} />
+          <Route path="detalhes-pedido" element={<DetalhesDoPedido />} />
+          <Route path="historico" element={<HistoricoPedidos />} />
+          <Route path="configuracoes" element={<Configuracoes />} />
+          <Route path="notificacoes" element={<NotificacoesSolicitante />} />
+          <Route path="*" element={<NotFoundSolicitante />} />
+        </Route>
+      </Route>
 
-            {/*Rotas do entregador */}
-            <Route path="/dashboard/entregador/">
-                <Route path="" element={<DashboardEntregador />} />
-                <Route path="lista-pedidos" element={<ListaDePedidos />} />
-                <Route path="detalhes-corrida" element={<DetalhesCorrida />} />
-                <Route path="historico" element={<HistoricoEntregador />} />
-                <Route path="notificacoes" element={<NotificacoesEntregador />} />
-                <Route path="configuracoes" element={<ConfiguracoesEntregador />} />
-                
-                <Route path="*" element={<NotFoundEntregador />} />
-            </Route>
-        </Routes>
-    )
+      {/* Entregador */}
+      <Route element={<RolePrivateRoute allowedRoles={["ENTREGADOR"]} />}>
+        <Route path="/dashboard/entregador">
+          <Route index element={<DashboardEntregador />} />
+          <Route path="lista-pedidos" element={<ListaDePedidos />} />
+          <Route path="detalhes-corrida" element={<DetalhesCorrida />} />
+          <Route path="historico" element={<HistoricoEntregador />} />
+          <Route path="notificacoes" element={<NotificacoesEntregador />} />
+          <Route path="configuracoes" element={<ConfiguracoesEntregador />} />
+          <Route path="*" element={<NotFoundEntregador />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
