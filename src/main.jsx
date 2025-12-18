8f378b41499f4d1cd16140a171b35b70659688fa
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-12-18 07:37:56 +0100

    AuthProvider adicionado para proteção de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import 'leaflet/dist/leaflet.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "leaflet/dist/leaflet.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./contexts/AuthContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Toaster position="top-right" />
-      <AppRoutes />
+      <AuthProvider>
+        <Toaster position="top-right" />
+        <AppRoutes />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
-)
+);
