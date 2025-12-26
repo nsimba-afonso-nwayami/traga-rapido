60b874b3bd1ebee09986196d1463e089df7ccb4f
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-12-26 04:32:55 +0100

    Validação do login atualizado

--- src/validations/loginSchema.js
+++ src/validations/loginSchema.js
@@ -1,8 +1,9 @@
 import * as Yup from "yup";
 
 export const loginSchema = Yup.object({
-  username: Yup.string()
-    .required("O nome de usuário é obrigatório"),
+  email: Yup.string()
+    .email("Digite um email válido")
+    .required("O email é obrigatório"),
 
   password: Yup.string()
     .required("A senha é obrigatória")
